f7ea81cd6c9c4366bda5c642f8fbd5c502541f79
Vlad <vladsilagy@gmail.com>
2022-11-16 17:18:38 -0500

    add axios, fetch API, add loading gif

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { BsSearch } from 'react-icons/bs';
+import Loading from './components/Loading';
 function App() {
+	const [weather, setWeather] = useState('');
+	const [location, setLocation] = useState('Stamford');
+	const [loading, setLoading] = useState(false);
+
+	const key = 'd3b5cd690817fe32be11dd566e32f1ca';
+
+	useEffect(() => {
+		setLoading(true);
+
+		const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}`;
+
+		axios.get(url).then((response) => {
+			console.log(response.data);
+			setWeather(response.data);
+		});
+		setLoading(false);
+	}, [location]);
+	console.log(weather);
 	return (
 		<div>
-			<h1 className="text-red-500">Weather App</h1>
+			<h1 className="text-red-500">{loading && <Loading />}</h1>
 		</div>
 	);
 }
---
+++ src/components/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import hurricane from '../assets/hurricane.svg';
+
+const Loading = () => {
+	return (
+		<>
+			<img src={hurricane} alt="loading" className="w-48 m-auto" />
+		</>
+	);
+};
+
+export default Loading;
