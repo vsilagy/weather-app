29ec8549569075d8a777ce620cafa521966cfef3
Vlad <vladsilagy@gmail.com>
2022-11-16 20:53:16 -0500

    add raining gif while loading

--- src/App.jsx
+++ src/App.jsx
@@ -30,11 +30,11 @@ function App() {
 	}, [location]);
 
 	return (
-		<div className="h-screen flex flex-col gap-5 justify-center items-center bg-slate-400">
+		<div className="h-screen flex flex-col gap-5 justify-center items-center Pastel bg-gradient-to-tr from-violet-500 to-orange-300">
 			<form onSubmit={handleSubmit}>
 				<div className="w-80 md:w-[32rem]">
 					<input
-						className="w-full py-4 px-2 text-white text-lg rounded outline-none shadow-md bg-slate-600 placeholder:text-gray-400"
+						className="w-full py-4 px-2 text-white text-lg rounded outline-none shadow-md bg-transparent/30 placeholder:text-gray-400"
 						type="text"
 						value={input}
 						onChange={(e) => handleInput(e)}
@@ -42,51 +42,60 @@ function App() {
 					/>
 				</div>
 			</form>
-			{loading ? (
-				<Loading />
-			) : (
-				<section className="w-80 md:w-[32rem] py-4 px-2 grid grid-cols-2 text-white text-lg rounded outline-none shadow-md bg-slate-600">
-					<div>
-						{data.weather ? <p>{data.weather[0].main}</p> : null}
-					</div>
-					<div>
-						<h1 className="text-3xl ">{data.name}</h1>
-					</div>
 
-					<div>
-						{data.main ? (
-							<h1>{data.main.temp.toFixed()} °C</h1>
-						) : null}
+			<section className="w-80 md:w-[32rem] py-4 px-2 grid grid-cols-2 text-white text-lg rounded outline-none shadow-md bg-transparent/30">
+				{loading ? (
+					<div className="col-span-2">
+						<Loading />
 					</div>
-					<div>
-						{data.main ? <p>{data.weather[0].description}</p> : null}
-					</div>
-					<div>
-						<p className="ml-2">{data.visibility / 1000} km</p>
-						<p>Visibility</p>
-					</div>
-					<div>
-						{data.main ? (
-							<p className="bold">
-								{data.main.feels_like.toFixed()}°C
-							</p>
-						) : null}
-						<p>Feels Like</p>
-					</div>
-					<div>
-						{data.main ? (
-							<p className="bold">{data.main.humidity}%</p>
-						) : null}
-						<p>Humidity</p>
-					</div>
-					<div>
-						{data.wind ? (
-							<p className="bold">{data.wind.speed.toFixed()} MPH</p>
-						) : null}
-						<p>Wind Speed</p>
-					</div>
-				</section>
-			)}
+				) : (
+					<>
+						<div>
+							{data.weather ? <p>{data.weather[0].main}</p> : null}
+						</div>
+						<div>
+							<h1 className="text-3xl ">{data.name}</h1>
+						</div>
+
+						<div>
+							{data.main ? (
+								<h1>{data.main.temp.toFixed()} °C</h1>
+							) : null}
+						</div>
+						<div>
+							{data.main ? (
+								<p>{data.weather[0].description}</p>
+							) : null}
+						</div>
+						<div>
+							<p className="ml-2">{data.visibility / 1000} km</p>
+							<p>Visibility</p>
+						</div>
+						<div>
+							{data.main ? (
+								<p className="bold">
+									{data.main.feels_like.toFixed()}°C
+								</p>
+							) : null}
+							<p>Feels Like</p>
+						</div>
+						<div>
+							{data.main ? (
+								<p className="bold">{data.main.humidity}%</p>
+							) : null}
+							<p>Humidity</p>
+						</div>
+						<div>
+							{data.wind ? (
+								<p className="bold">
+									{data.wind.speed.toFixed()} MPH
+								</p>
+							) : null}
+							<p>Wind Speed</p>
+						</div>
+					</>
+				)}
+			</section>
 		</div>
 	);
 }
--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import hurricane from '../assets/hurricane.svg';
+import rain from '../assets/rain.svg';
 
 const Loading = () => {
 	return (
 		<>
-			<img src={hurricane} alt="loading" className="w-48 m-auto" />
+			<img src={rain} alt="loading" className="w-48 md:w-64 m-auto" />
 		</>
 	);
 };
