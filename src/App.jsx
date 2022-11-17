5d8642792bb59edaac565dbcab4a58bb762cfb54
Vlad <vladsilagy@gmail.com>
2022-11-16 20:32:39 -0500

    add loading gif

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,8 @@ function App() {
 	const [data, setData] = useState({});
 	const [location, setLocation] = useState('Stamford');
 	const [input, setInput] = useState('');
-	// const [loading, setLoading] = useState(false);
+	const [loading, setLoading] = useState(false);
+	const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${API_KEY}`;
 
 	const handleInput = (e) => {
 		setInput(e.target.value);
@@ -21,21 +22,17 @@ function App() {
 	};
 
 	useEffect(() => {
-		// setLoading(true);
-
-		const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${API_KEY}`;
-
+		setLoading(true);
 		axios.get(url).then((res) => {
 			setData(res.data);
-			console.log(data);
+			setLoading(false);
 		});
-		// setLoading(false);
 	}, [location]);
 
 	return (
 		<div className="h-screen flex flex-col gap-5 justify-center items-center bg-slate-400">
 			<form onSubmit={handleSubmit}>
-				<div className="w-96">
+				<div className="w-80 md:w-[32rem]">
 					<input
 						className="w-full py-4 px-2 text-white text-lg rounded outline-none shadow-md bg-slate-600 placeholder:text-gray-400"
 						type="text"
@@ -45,43 +42,51 @@ function App() {
 					/>
 				</div>
 			</form>
-			<section className="w-96 py-4 px-2 grid grid-cols-2 text-white text-lg rounded outline-none shadow-md bg-slate-600">
-				<div>
-					{data.weather ? <p>{data.weather[0].main}</p> : null}
-				</div>
-				<div>
-					<h1 className="text-3xl ">{data.name}</h1>
-				</div>
+			{loading ? (
+				<Loading />
+			) : (
+				<section className="w-80 md:w-[32rem] py-4 px-2 grid grid-cols-2 text-white text-lg rounded outline-none shadow-md bg-slate-600">
+					<div>
+						{data.weather ? <p>{data.weather[0].main}</p> : null}
+					</div>
+					<div>
+						<h1 className="text-3xl ">{data.name}</h1>
+					</div>
 
-				<div>
-					{data.main ? <h1>{data.main.temp.toFixed()} °C</h1> : null}
-				</div>
-				<div>
-					{data.main ? <p>{data.weather[0].description}</p> : null}
-				</div>
-				<div>
-					<p className="ml-2">{data.visibility / 1000} km</p>
-					<p>Visibility</p>
-				</div>
-				<div>
-					{data.main ? (
-						<p className="bold">{data.main.feels_like.toFixed()}°C</p>
-					) : null}
-					<p>Feels Like</p>
-				</div>
-				<div>
-					{data.main ? (
-						<p className="bold">{data.main.humidity}%</p>
-					) : null}
-					<p>Humidity</p>
-				</div>
-				<div>
-					{data.wind ? (
-						<p className="bold">{data.wind.speed.toFixed()} MPH</p>
-					) : null}
-					<p>Wind Speed</p>
-				</div>
-			</section>
+					<div>
+						{data.main ? (
+							<h1>{data.main.temp.toFixed()} °C</h1>
+						) : null}
+					</div>
+					<div>
+						{data.main ? <p>{data.weather[0].description}</p> : null}
+					</div>
+					<div>
+						<p className="ml-2">{data.visibility / 1000} km</p>
+						<p>Visibility</p>
+					</div>
+					<div>
+						{data.main ? (
+							<p className="bold">
+								{data.main.feels_like.toFixed()}°C
+							</p>
+						) : null}
+						<p>Feels Like</p>
+					</div>
+					<div>
+						{data.main ? (
+							<p className="bold">{data.main.humidity}%</p>
+						) : null}
+						<p>Humidity</p>
+					</div>
+					<div>
+						{data.wind ? (
+							<p className="bold">{data.wind.speed.toFixed()} MPH</p>
+						) : null}
+						<p>Wind Speed</p>
+					</div>
+				</section>
+			)}
 		</div>
 	);
 }
